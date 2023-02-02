613756011988ede2efacf4b1482cae6dd1e44cb5
Ococho <70449743+Ococho@users.noreply.github.com>
2023-02-02 16:06:39 -0500

    :tada: Initial commit. Initializing project Viajes Ococho

---
+++ src/main/java/com/sofka/co/ViajesOcochoApp.java
@@ -0,0 +1,13 @@
+package com.sofka.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesOcochoApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesOcochoApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/co/ViajesOcochoAppTests.java
@@ -0,0 +1,13 @@
+package com.sofka.co;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesOcochoAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
