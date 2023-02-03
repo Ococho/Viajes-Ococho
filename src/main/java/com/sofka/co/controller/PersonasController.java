1927c9ea727b5e28c797ffdd7d88da92b20a9857
Ococho <jonathan.vargas85@gmail.com>
2023-02-03 17:49:52 -0500

    :sparkles: Adding functionality to PersonasController

--- src/main/java/com/sofka/co/controller/PersonasController.java
+++ src/main/java/com/sofka/co/controller/PersonasController.java
@@ -1,12 +1,11 @@
 package com.sofka.co.controller;
 
+import com.sofka.co.model.frame.Persona;
 import com.sofka.co.service.IPersonasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("clientes")
@@ -15,8 +14,21 @@ public class PersonasController {
     @Autowired
     private IPersonasService personasService;
 
-//    @GetMapping()
-//    public ResponseEntity obtenerPersonas() {
-//        return new ResponseEntity(personasService., HttpStatus.FOUND);
-//    }
+    @PostMapping()
+    public ResponseEntity registrarPersona(@RequestBody Persona persona) {
+        personasService.registrarPersona(persona);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity obtenerPersonas() {
+        return new ResponseEntity(personasService.obtenerPersonas(), HttpStatus.FOUND);
+    }
+
+    @PutMapping()
+    public ResponseEntity modificarPersona(@RequestBody Persona personaAModificar) {
+        return new ResponseEntity(personasService.modificarPersona(personaAModificar), HttpStatus.ACCEPTED);
+    }
+
+
 }
